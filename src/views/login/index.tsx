d4e3e18359d5fdc7a3d231fea769b441904ead13
蒋军晖 <mobeigege@icloud.com>
2022-04-21 12:22:15 +0800

    test workflow

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,75 +1,68 @@
 // noinspection JSIgnoredPromiseFromCall
 
-import React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
-import './index.scss'
-import { Form, Input, Button, Typography, message } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
-import Axios from '../../utils/http'
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import './index.scss';
+import { Form, Input, Button, Typography, message } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import Axios from '../../utils/http';
 
-
-const { Title } = Typography
+const { Title } = Typography;
 export default class Login extends React.Component<RouteComponentProps> {
-	constructor(props: RouteComponentProps) {
-		super(props)
-	}
+    constructor(props: RouteComponentProps) {
+        super(props);
+    }
 
-	onFinish = (values: any) => {
-		Axios('/api/user/login', {
-			method: 'POST',
-			responseType: 'json',
-			data: {
-				...values
-			}
-		}).then((response) => {
-			const { code, message: msg } = response.data
-			if (code === 200) {
-				if (response.data.token) {
-					message.success(msg)
-					localStorage.setItem('user', JSON.stringify(response.data.user))
-					localStorage.setItem('token', response.data.token)
-					this.props.history.push('/home')
-				} else {
-					message.warning(msg)
-				}
-			} else {
-				message.error('报错了')
-			}
-		})
-	}
+    onFinish = (values: any) => {
+        Axios('/api/user/login', {
+            method: 'POST',
+            responseType: 'json',
+            data: {
+                ...values
+            }
+        }).then((response) => {
+            const { code, message: msg } = response.data;
+            if (code === 200) {
+                if (response.data.token) {
+                    message.success(msg);
+                    localStorage.setItem('user', JSON.stringify(response.data.user));
+                    localStorage.setItem('token', response.data.token);
+                    this.props.history.push('/home');
+                } else {
+                    message.warning(msg);
+                }
+            } else {
+                message.error('报错了');
+            }
+        });
+    };
 
-	render() {
-		return (
-			<>
-				<Title level={2}>这就是个登录页</Title>
-				<Form
-					name='normal_login'
-					className='login-form'
-					initialValues={{ remember: true }}
-					onFinish={this.onFinish}>
-					<Form.Item name='username'
-					           rules={[{ required: true, message: '请输入用户名!' }]}>
-						<Input prefix={<UserOutlined
-							className='site-form-item-icon' />}
-						       placeholder='用户名' />
-					</Form.Item>
-					<Form.Item name='password'
-					           rules={[{ required: true, message: '请输入密码!' }]}>
-						<Input
-							prefix={<LockOutlined
-								className='site-form-item-icon' />}
-							type='password'
-							placeholder='密码'
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Button type='primary' htmlType='submit'
-						        className='login-form-button'>
-							登录
-						</Button>
-					</Form.Item>
-				</Form>
-			</>
-		)
-	}
+    render() {
+        return (
+            <>
+                <Title level={2}>这就是个自动登录页</Title>
+                <Form
+                    name='normal_login'
+                    className='login-form'
+                    initialValues={{ remember: true }}
+                    onFinish={this.onFinish}>
+                    <Form.Item name='username' rules={[{ required: true, message: '请输入用户名!' }]}>
+                        <Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='用户名' />
+                    </Form.Item>
+                    <Form.Item name='password' rules={[{ required: true, message: '请输入密码!' }]}>
+                        <Input
+                            prefix={<LockOutlined className='site-form-item-icon' />}
+                            type='password'
+                            placeholder='密码'
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit' className='login-form-button'>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </>
+        );
+    }
 }
